5f62260c9c9714510fe571053441eff11ef890bf
Shivam00123 <shivamjackson6@gmail.com>
2024-02-19 22:11:19 +0530

    added file

---
+++ src/Screen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Screen = () => {
+  return (
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      learn react
+    </a>
+  );
+};
+
+export default Screen;
